6083f93df3244a0942f8bc2351f2a688abb61e11
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-20 12:33:14 +0530

    java program

---
+++ Details.java
@@ -0,0 +1,17 @@
+public class Details
+{
+public static void main(String[] args)
+{
+System.out.println("Details about gmail and skills of programming language");
+printEmail("Surakshita.xworkz@gmail.com");
+skill("JAVA");
+}
+public static void printEmail(String EmailId)
+{
+System.out.println("EmailId:"+EmailId);
+}
+public static void skill(String ProgrammingLang)
+{
+System.out.println("ProgrammingLang:"+ProgrammingLang);
+}
+}
