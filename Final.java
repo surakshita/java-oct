98de71083d382d27e47e56504a5ba3c4f09939a3
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-20 12:33:46 +0530

    java program

---
+++ Final.java
@@ -0,0 +1,19 @@
+public class Final
+{
+public static void main(String[] args)
+{
+System.out.println("Details with the final keyword");
+
+final String aadharNumber;
+aadharNumber="444433332222";
+System.out.println("aadharNumber:"+aadharNumber);
+
+final String phoneNumber;
+phoneNumber="9448146***";
+System.out.println("phoneNumber:"+phoneNumber);
+
+final String Email;
+Email="Surakshita.xworkz@gmail.com";
+System.out.println("Email:"+Email);
+}
+}
